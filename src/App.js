fad1f9bf7412d5ee15da6763af0dff489f6470e6
antu kumar <antu9649@gmail.com>
2021-07-22 11:08:31 +0530

    api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {useState} from "react";
+import {useEffect } from 'react';
+
 
 function App() {
+
+        const [users, setUsers] = useState([])
+
+        const getUsers= async()=>{
+          const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+         setUsers(await response.json());
+         //const data = await response.json();
+         //console.log(data);
+
+         
+     
+        }
+
+        useEffect(()=>{
+          getUsers();
+        }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+
+
+
+      
+     <div className="main">
+
+     <table className="table">
+    
+           <thead>
+             <tr>
+                <th>S.No</th>
+                <th>Title</th>
+                <th>Body</th>
+                
+               
+               
+             </tr>
+             </thead>
+
+
+             {
+                users.map((curr) => {
+                 return(
+                   
+                        
+                      <tbody>
+                          <tr>
+                      <td>{curr.id}</td>
+                            <td>{curr.title}</td>
+                            <td>{curr.body}</td>
+                           
+                          
+
+                          </tr>
+                      </tbody>
+                    
+                   
+                 )
+
+               })
+             }
+
+           
+      </table>
+
+       </div>
+  
+
+
+   </>
   );
 }
 
